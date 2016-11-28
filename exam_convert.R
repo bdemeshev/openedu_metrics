5bd5cceba08e5c8daab3e138281bca1d855e7074
Boris Demeshev <boris.demeshev@gmail.com>
2016-11-28 16:59:28 +0300

    template from Rmd built in

--- exam_convert.R
+++ exam_convert.R
@@ -1,28 +1,44 @@
+# library("exams")
+# devtools::install_github("bdemeshev/exams")
+
 library("exams")
 library("tidyverse")
+library("stringr")
 
-devtools::install_github("bdemeshev/exams")
 
-mydir <- "~/Documents/openedu_metrics/moodle3"
+mydir <- "~/Documents/openedu_metrics/moodle_exam"
 setwd(mydir)
 
-exams_skeleton(dir = mydir, encoding = "UTF-8", markup = "markdown")
-
-?exams.skeleton
+# exams_skeleton(dir = mydir, encoding = "UTF-8", markup = "markdown")
 
 # латеховские формулы в $...$ и в $$..$$
 # для картинок обязательно выставить fig.path = ""
 
-set.seed(1090)
+set.seed(42)
+
+# openedu_final_v01 <- c("exercises/swisscapital2.Rmd", "exercises/boxhist.Rmd")
+# openedu_final_v01 <- c("exercises/swisscapital2.Rmd",  "exercises/v02_q01.Rmd")
+
+all_rmd <- list.files("exercises/")
+
+openedu_final_v01 <- str_subset(all_rmd, pattern = "^v01")
+openedu_final_v01 <- paste0("exercises/", openedu_final_v01)
+
+dir.create("moodle_out")
+output_dir <- "moodle_out/"
+exams2moodle(openedu_final_v01, n = 1, dir = output_dir, encoding = "UTF-8")
+# n - number of copies
 
 
 
-openedu_final_v01 <- c("exercises/swisscapital2.Rmd", "exercises/boxhist.Rmd")
-openedu_final_v01 <- c("exercises/swisscapital2.Rmd",  "exercises/v02_q01.Rmd")
+dir.create("pdf_out")
+dir.create("pdf_out_temp")
+dir.create("pdf_out_supp")
+
 
 exams2pdf(openedu_final_v01, encoding = "UTF-8", template = "templates/plain.tex",
           verbose = TRUE, dir = "pdf_out", tdir = "pdf_out_temp", sdir = "pdf_out_supp")
-exams2html(openedu_final_v01, encoding = "UTF-8", template = "plain8")
+exams2html(openedu_final_v01, encoding = "UTF-8", template = "templates/plain.tex")
 
 exams2nops(openedu_final_v01,
            language = "ru", encoding = "UTF-8")
@@ -30,18 +46,7 @@ exams2nops(openedu_final_v01,
 # produces md from Rmd
 xweave(openedu_final_v01, encoding = "UTF-8", engine = "knitr")
 
-?xweave
-?exams2pdf
-?exams2nops
 
-output_dir <- "moodle_out/"
 
-pdfwrite <- make_exams_write_pdf()
-a <- xexams(openedu_final_v01, driver = list(write = pdfwrite))
-
-exams2moodle(openedu_final_v01, n = 1, dir = output_dir, encoding = "UTF-8")
-# n - number of copies
-
-.libPaths()
 
 
