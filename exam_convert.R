bbeb5161d98126673b4fcc12b928efa5c6cda1bf
Boris Demeshev <boris.demeshev@gmail.com>
2017-02-18 15:51:46 +0300

    misprint

--- exam_convert.R
+++ exam_convert.R
@@ -16,8 +16,8 @@ setwd(mydir)
 
 set.seed(42)
 
-# openedu_final_v01 <- c("exercises/swisscapital2.Rmd", "exercises/boxhist.Rmd")
-# openedu_final_v01 <- c("exercises/swisscapital2.Rmd",  "exercises/v02_q01.Rmd")
+openedu_final <- c("exercises/v02_q01.Rmd", "exercises/v02_q02.Rmd", "exercises/v02_q03.Rmd")
+# openedu_final <- c("exercises/swisscapital2.Rmd",  "exercises/v02_q01.Rmd")
 
 all_rmd <- list.files("exercises/")
 
@@ -36,15 +36,18 @@ dir.create("pdf_out_temp")
 dir.create("pdf_out_supp")
 
 
-exams2pdf(openedu_final, encoding = "UTF-8", template = "templates/plain.tex",
+exams2pdf(openedu_final, encoding = "UTF-8", template = "templates/solution.tex",
           verbose = TRUE, dir = "pdf_out", tdir = "pdf_out_temp", sdir = "pdf_out_supp")
-exams2html(openedu_final, encoding = "UTF-8", template = "templates/plain.tex")
 
-exams2nops(openedu_final,
-           language = "ru", encoding = "UTF-8")
+
+
+# exams2html(openedu_final, encoding = "UTF-8", template = "templates/plain.tex")
+
+# exams2nops(openedu_final,
+#           language = "ru", encoding = "UTF-8")
 
 # produces md from Rmd
-xweave(openedu_final, encoding = "UTF-8", engine = "knitr")
+# xweave(openedu_final, encoding = "UTF-8", engine = "knitr")
 
 
 
